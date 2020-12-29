8c69653151ff3a40aeb7b97ee3ac1238d045019e
truong0806 <thanhtruong090123@gmail.com>
2020-12-30 00:16:17 +0700

    Mới đổi

--- Quanlytrangtrai/Program.cs
+++ Quanlytrangtrai/Program.cs
@@ -7,9 +7,7 @@ namespace Quanlytrangtrai
         static void Main(string[] args)
         {
             FarmingController Controll = new FarmingController();
-            Console.WriteLine("Ten con vat can them: ");
-            string name = Console.ReadLine();
-            Console.WriteLine("So luong them: ");
+            Console.WriteLine("Hello");
         }
     }
 }
